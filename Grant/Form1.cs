136dddd136723c536075681f68722b42afc4a524
Bilale <bilale.harouna_abassi@devinci.fr>
2016-01-14 09:38:51 +0100

    test2

--- Grant/Form1.cs
+++ Grant/Form1.cs
@@ -21,6 +21,7 @@ namespace Grant
             // create built-in task or use your own derived task
             var task = new Braincase.GanttChart.Task() { Name = "Hello World"};
             var task1 = new Braincase.GanttChart.Task() { Name = " World" };
+            var task2 = new Braincase.GanttChart.Task() { Name = "Hello" };
             
             // add the task to the manager
             manager.Add(task);
