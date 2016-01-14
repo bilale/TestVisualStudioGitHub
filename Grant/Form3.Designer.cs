cbde4ec3eda4806e73345db8c64eafec8bcc2382
Bilale <bilale.harouna_abassi@devinci.fr>
2016-01-14 13:04:55 +0100

    zed

--- Grant/Form3.Designer.cs
+++ Grant/Form3.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.Name = new System.Windows.Forms.ListBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Name
@@ -50,11 +51,21 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "linkLabel1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.Name);
             this.Name = "Form3";
@@ -68,5 +79,6 @@
 
         private System.Windows.Forms.ListBox Name;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
     }
 }
